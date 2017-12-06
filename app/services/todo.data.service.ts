51b9056db3911247a7b70229851152d85e604cba
Mary <marysaphpir@gmail.com>
2017-12-06 16:31:00 +0200

    rm folder

--- app/services/todo.data.service.ts
+++
@@ -1,7 +0,0 @@
-import { Todo } from './todo.data';
-
-export const todos: Todo[] = [
-  {title: 'todo 1', completed: true},
-  {title: 'todo 2', completed: false},
-  {title: 'todo 3', completed: false}
-];
--- app/todo-list/filter.todo.pipe.ts
+++
@@ -1,9 +0,0 @@
-import {Pipe, PipeTransform} from '@angular/core';
-import {Todo} from '../services/todo.data';
-
-@Pipe({name: 'getCompletedTodo'})
-export class FilterTodoPipe implements PipeTransform {
-  transform(completedTodos: Todo[]) {
-    return completedTodos.filter(todo => todo.completed);
-  }
-}
